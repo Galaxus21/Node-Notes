8aac2448c32db0623f78c29f60082a77d600e8e7
Shubham Verma <vermaprince2004@gmail.com>
2025-03-25 19:37:53 +0530

    filler commit

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/add-user',(req, res, next)=>{
+    res.send("<form action='/user' method='post'><input type='text' name='user'/><button type='submit'>submit</button></form>")
+})
+
+app.use('/user', (req, res, next)=>{
+    const data = req.body;
+    res.redirect('/');   //Redirects to a different page.
+})
+
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Hello from Express !!!</h1>');
+})
+
+app.listen(3000)
+
+// const server = http.createServer(app);
+// server.listen(3000);
