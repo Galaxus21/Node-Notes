859f74e01ec4617f9aef4cf5584e7f46a03baf2c
Shubham Verma <vermaprince2004@gmail.com>
2025-03-30 20:42:00 +0530

    cleanup

--- Express/note.js
+++ Express/note.js
@@ -5,3 +5,17 @@
         To install them all at once - npm install ejs pug express-handlebars
      
 */
+
+/* To Setup handlebars
+app.engine('hbs', expressHbs({
+    layoutsDir: 'views/layouts/', 
+    defaultLayout: 'main-layout',
+    extname: 'hbs' //Applies to only layout file.
+}));
+app.set('view engine', 'hbs'); //The extension for handlebars template will also be the same(hbs).
+*/
+
+/* To Setup pug
+app.set('view engine', 'pug'); //Sets the default engine as pug.
+app.set('views', 'views');  // Used to set the path of the templates, by default it is 'views'.
+*/
